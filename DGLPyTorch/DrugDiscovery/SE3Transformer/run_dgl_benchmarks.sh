f0600253398c208ffd9321e31ad89a6bea9cd4cd
mukh1l <mmallaiy@amd.com>
2025-05-05 15:43:28 +0000

    additional changes to handle datatypes

--- DGLPyTorch/DrugDiscovery/SE3Transformer/run_dgl_benchmarks.sh
+++ DGLPyTorch/DrugDiscovery/SE3Transformer/run_dgl_benchmarks.sh
@@ -2,3 +2,11 @@
 
 bash scripts/benchmark_train.sh 120
 bash scripts/benchmark_train.sh 240
+bash scripts/benchmark_train.sh 120 --amp
+bash scripts/benchmark_train.sh 240 --amp
+bash scripts/benchmark_train_multi_gpu.sh 120
+bash scripts/benchmark_train_multi_gpu.sh 240
+bash scripts/benchmark_train_multi_gpu.sh 120 --amp
+bash scripts/benchmark_train_multi_gpu.sh 240 --amp
+bash scripts/benchmark_inference.sh 400
+bash scripts/benchmark_inference.sh 400 --amp 
