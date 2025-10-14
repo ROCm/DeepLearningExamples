64c9193ff2f58146c9c498c5dee21d4f42e990bb
jamesETsmith <james.smith9113@gmail.com>
2025-10-14 15:34:00 -0400

    Updating run_dgl_benchmarks.sh so it runs on amd or nvidia

--- DGLPyTorch/DrugDiscovery/SE3Transformer/run_dgl_benchmarks.sh
+++ DGLPyTorch/DrugDiscovery/SE3Transformer/run_dgl_benchmarks.sh
@@ -5,7 +5,14 @@ set -euo pipefail
 # Log file
 LOGFILE="dgl_se3_benchmarks_$(hostname).log"
 echo "Running benchmarks on $(hostname)" > $LOGFILE
-amd-smi static | grep "MARKET_NAME" | tee -a $LOGFILE
+
+# Print GPU-specific information
+if [ $(which amd-smi) ]; then
+    amd-smi static | grep "MARKET_NAME" | tee -a $LOGFILE
+elif [ $(which nvidia-smi) ]; then
+    nvidia-smi | tee -a $LOGFILE
+fi
+
 echo "DGL version: $(python -c "import dgl; print(dgl.__version__)")" | tee -a $LOGFILE
 
 # Run benchmarks
